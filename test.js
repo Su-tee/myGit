e35ddbdc97fb068397a11120ba227ad22c21819c
Su-tee <suteepuangchomphu@gmail.com>
2023-07-08 16:50:22 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-console.log("test");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get("/",(req,res) => {
+    res.send('hello')
+})
+
+app.listen(port, ()=>{
+    console.log("listening on port %d",port)
+})
